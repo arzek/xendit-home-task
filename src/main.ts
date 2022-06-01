e8fdd9dabe4654ed8c7a58529745ffeb0378110c
Artem Bondarchuk <artem@undock.com>
2022-06-01 16:42:55 +0800

    add test:load

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { TransformInterceptor } from './modules/common/transform.interceptor';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+
+import helmet from 'helmet';
+
+import { TransformInterceptor } from './modules/common/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
 
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
